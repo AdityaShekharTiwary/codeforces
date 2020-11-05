ca50bc42cce5b08272776eb9f5b6141074077544
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-05 18:56:26 +0530

    Integer Sequence dividing
    https://codeforces.com/contest/1102/problem/A

---
+++ cf_integer_sequence_diviving.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum+=i;
+    }
+    if(sum%2==0)
+        cout<<0<<endl;
+    else
+        cout<<1<<endl;
+    return 0;
+}
