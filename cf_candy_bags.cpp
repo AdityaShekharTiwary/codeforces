e4d5fa4edf73ec3b1551b93ffa8c5b7f4f0f4527
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-11-21 15:51:44 +0530

    Candy bags

---
+++ cf_candy_bags.cpp
@@ -0,0 +1,35 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cout<<i+1;
+        for(int j=1;j<i;j++)
+        {
+            cout<<n*j+(i+j)%n+1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}*/
+
+#include <cstdio>
+#include<iostream>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+    {
+        printf("%d", i + 1);
+        for (int j = 1; j < n; ++j)
+        {
+            printf(" %d", n * j + (i + j) % n + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
