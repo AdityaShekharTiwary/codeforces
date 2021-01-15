2334aa819c3162a06dc42e7a03b348dcf3e1f224
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-15 19:43:07 +0530

    No more inversion

---
+++ cf_no_more_inversion.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;cin>>t;
+	while(t--){
+		int n,k;cin>>n>>k;
+		for(int i=1;i<k-(n-k);i++)cout<<i<<" ";
+		for(int i=k;i>=2*k-n;i--)cout<<i<<" ";
+		cout<<endl;
+	}
+}
