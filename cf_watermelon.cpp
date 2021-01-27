9a816bae8d03461cce6a8f25c0f1b475a37a4c6f
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-27 21:02:00 +0530

    Watermelon

---
+++ cf_watermelon.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define v vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define m map<int,int>
+#define mll map<ll,ll>
+#define pb push_back
+#define test int t; cin>>t; while(t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+int main()
+{
+	fast
+	//test
+	{
+        int n,flag=0;cin>>n;
+        if(n==2) flag=0;
+        else if(n%2==0)flag=1;
+        else flag=0;
+        if(flag==0) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+	}
+
+	return 0;
+}
+
