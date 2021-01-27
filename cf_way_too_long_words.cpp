906f765b8f7359a681927e26aa6f7655f8d39074
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-27 21:02:29 +0530

    Way too long words

---
+++ cf_way_too_long_words.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define v vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define m map<int,int>
+#define mll map<ll,ll>
+#define pb push_back
+#define test int t; cin>>t; while(t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+int main()
+{
+	fast
+	test
+	{
+        string s;cin>>s;
+        int n=s.size();
+        if(n>10) cout<<s[0]<<n-2<<s[n-1]<<endl;
+        else cout<<s<<endl;
+	}
+
+	return 0;
+}
+
