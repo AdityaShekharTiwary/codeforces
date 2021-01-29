bf3d290dee271be28aca243e80472ab243462156
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-01-29 23:34:10 +0530

    Test

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define v vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define m map<int,int>
+#define mll map<ll,ll>
+#define pb push_back
+#define test int t; cin>>t; while(t--)
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+int main()
+{
+	fast
+	test
+	{
+        float n, k;
+        //int i=2;
+        cin>>n>>k;
+        int r=k;
+        while(n>k){
+            k+=r;
+        }
+        if(k/n==(int)(k/n))
+            cout<<k/n<<endl;
+        else
+            cout<<(int)(k/n)+1<<endl;
+	}
+
+	return 0;
+}
