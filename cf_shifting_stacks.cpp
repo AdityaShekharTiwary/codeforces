8891ebbcf7c99f8d3e95cf7c728c2499f7c2c966
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-02-18 21:52:14 +0530

    Shifting stack

---
+++ cf_shifting_stacks.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define MAX_VAL 2^31-1
+#define vll vector<ll>
+#define vi vector<int>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define fo(i,s,e) for(int i=s;i<=e;i++)
+#define mi map<int,int>
+#define mp make_pair
+#define pll pair<ll,ll>
+#define mll map<ll,ll>
+#define pb push_back
+#define ff first
+#define ss second
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input1.txt","r",stdin);
+		freopen("output1.txt","w",stdout);
+	#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+
+void solve()
+{
+	int n; cin>>n;
+	vll arr(n);
+	int s=0;
+	bool f=true;
+    f(i,0,n){
+        cin>>arr[i];
+        s+=arr[i];
+        if(s<((i)*(i+1))/2) f=false;
+    }
+        if(f)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+}
+
+void help()
+{
+	int n; cin>>n;
+	vll arr(n);
+	input_arr
+	int flag=0;
+	f(i,0,n){
+		if(arr[i]>=arr[i+1]){
+			while(arr[i]>=arr[i+1] && arr[i]!=0){
+				arr[i]--;
+				arr[i+1]++;
+			}
+		}
+	}
+	f(i,0,n) {
+		if(arr[i]>=arr[i+1]) flag=1;
+	}
+	if(flag==1) cout<<"NO"<<endl;
+	else cout<<"YES"<<endl;
+}
+
+int main()
+{
+	fast();
+
+	test
+	{
+    	solve();
+	}
+
+	return 0;
+}
