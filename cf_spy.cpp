92fbe6b8763258ff339a41730e845ad181d7f52f
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-04-10 22:50:05 +0530

    Spy

---
+++ cf_spy.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<ll>
+#define pb push_back
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input1.txt","r",stdin);
+		freopen("output1.txt","w",stdout);
+	#endif
+
+	fast_IO	
+	cout.tie(NULL);
+}
+
+void solve()
+{
+	int n; cin>>n;
+	vll arr(105); 
+	input_arr
+	vll a(105);
+	f(i,0,n) a[arr[i]]++;
+	for(int i=0;i<105;i++){
+		if(a[arr[i]]==1) cout<<i+1<<endl;
+	}
+
+}
+
+int main()
+{
+	fast();
+
+	test
+	{
+		solve();
+	}
+
+	return 0;
+}
