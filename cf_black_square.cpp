8e572b62632aac27fd9dfb9c105521afc6dbbc9e
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-04-16 10:22:44 +0530

    Black square

---
+++ cf_black_square.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define f(i,s,e) for(ll i=s;i<e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+
+void fast()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input1.txt","r",stdin);
+		freopen("output1.txt","w",stdout);
+	#endif
+
+	fast_IO	
+	cout.tie(NULL); 
+}
+
+
+void solve()
+{
+	vll arr(5);
+	f(i,0,4) cin>>arr[i];
+	string s; cin>>s;
+	int sum=0;
+	f(i,0,s.size()){
+		sum+=arr[s[i]-1-'0'];
+	}
+	cout<<sum<<endl;
+
+}
+
+int main()
+{
+	fast();
+
+	//test
+	{
+		solve();
+	}
+
+
+	return 0;
+}
