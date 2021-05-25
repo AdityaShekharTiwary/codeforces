cdaeb94ac254b9032b3a270fd027f2d5bc6144e8
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-05-25 21:27:47 +0530

    Same Difference

--- cf_same_diff.cpp
+++ cf_same_diff.cpp
@@ -1,16 +1,33 @@
 #include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
 #define ll long long int
-#define vll vector<ll>
 #define pb push_back
-#define mp make_pair
-#define debug(a) cout << a << " "
-#define Debug(a, b) cout << a << " " << b << '\n'
-#define input_arr for(int i=0;i<n;i++) cin>>arr[i];
+#define ff first
+#define ss second
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
 #define f(i,s,e) for(ll i=s;i<e;i++)
 #define test int t; cin>>t; while(t--)
 #define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
-using namespace std;
-
 
 void fast()
 {
@@ -18,27 +35,32 @@ void fast()
 #ifndef ONLINE_JUDGE
 	freopen("input1.txt", "r", stdin);
 	freopen("output1.txt", "w", stdout);
+	freopen("debug.txt", "w", stderr);
 #endif
 
 	fast_IO
 	cout.tie(NULL);
 }
 
+const ll mod = 1e9 + 7;
+
+
 void solve()
 {
-	int n; cin >> n;
-	int arr[n];
-	input_arr
-	int cnt = 0;
+	int n;
+	cin >> n;
+	map<int, int> a;
+	long long res = 0;
 	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			if (arr[j] - arr[i] == j - i) cnt++;
-		}
+		int x;
+		cin >> x;
+		x -= i;
+		res += a[x];
+		a[x]++;
 	}
-	cout << cnt << endl;
+	cout << res << endl;
 
 }
-
 
 int main()
 {
