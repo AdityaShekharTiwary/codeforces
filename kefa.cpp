ca082947bf0f361f89af370da08eb26b842a9e46
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-07-31 16:25:47 +0530

    Kefa

---
+++ kefa.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+#define nline "\n"
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+#define rep(i,s,e) for(ll i=s;i<e;i++)
+#define repp(i,s,e) for(ll i=s;i<=e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const ll mod = 1e9 + 7;
+
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    fast_IO
+    cout.tie(NULL);
+}
+
+
+
+void solve()
+{
+    ll n, d; cin >> n >> d;
+    vector<pair<ll, ll>> arr;
+    rep(i, 0, n) {
+        ll x, y; cin >> x >> y;
+        arr.pb({x, y});
+    }
+
+
+    sort(arr.begin(), arr.end());
+    debug(arr)
+
+    ll l = 0, res = 0, ans = INT_MIN, curr = 0;
+
+    rep(r, 0, n) {
+        curr += arr[r].ss;
+        while (arr[r].ff - arr[l].ff >= d) {
+            curr -= arr[l].ss;
+            l++;
+        }
+        ans = max(curr, ans);
+    }
+
+    cout << ans << nline;
+
+
+
+}
+
+
+int main()
+{
+    fast();
+
+    // test
+    {
+        solve();
+    }
+
+
+
+    return 0;
+}
+
+
+
