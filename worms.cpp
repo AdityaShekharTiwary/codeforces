898dc82cb098fc741eaf2c61d12d7f46a6f5a664
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-08-28 13:37:51 +0530

    Worms

---
+++ worms.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+#define nline "\n"
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define rep(i,s,e) for(int i=s;i<e;i++)
+#define repp(i,s,e) for(int i=s;i<=e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const ll mod = 1e9 + 7;
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    fast_IO
+    cout.tie(NULL);
+}
+
+int binarySearch(vector<int> arr, int l, int r, int x) {
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (arr[mid] < x) l = mid + 1;
+        else r = mid - 1;
+    }
+    return l;
+}
+
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> arr(n);
+    rep(i, 0, n) cin >> arr[i];
+    vector<int> prefix;
+    prefix.pb(arr[0]);
+    rep(i, 1, n) prefix.pb(arr[i] + prefix[i - 1]);
+    debug(prefix)
+    int q; cin >> q;
+    while (q--) {
+        int x; cin >> x;
+        cout << lower_bound(prefix.begin() , prefix.end() , x) - prefix.begin() + 1 << endl;
+    }
+
+
+}
+
+
+int main()
+{
+    fast();
+
+    // test
+
+    {
+        solve();
+    }
+
+
+
+    return 0;
+}
