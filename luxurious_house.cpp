6365a51334fdc9267216249d2dbec0b41f6a21a1
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2024-01-06 21:19:50 +0530

    Luxurious House

---
+++ luxurious_house.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+#define ll long long int
+#define pb push_back
+#define ff first
+#define ss second
+#define nline "\n"
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define rep(i,s,e) for(ll i=s;i<e;i++)
+#define repp(i,s,e) for(ll i=s;i<=e;i++)
+#define test int t; cin>>t; while(t--)
+#define fast_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+const ll mod = 1e9 + 7;
+
+void fast()
+{
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("debug.txt", "w", stderr);
+#endif
+
+	fast_IO
+	cout.tie(NULL);
+}
+
+void solve()
+{
+	int n; cin >> n;
+	vector<ll> arr(n), res;
+	rep(i, 0, n) cin >> arr[i];
+	res.pb(0);
+	int maxSoFar = arr[n - 1];
+	for (int i = n - 2; i >= 0; i--) {
+		if (arr[i] <= maxSoFar) res.pb(maxSoFar - arr[i] + 1);
+		else {
+			res.pb(0);
+			maxSoFar = arr[i];
+		}
+	}
+	reverse(res.begin(), res.end());
+	for (auto it : res) cout << it << " ";
+}
+
+
+
+int main()
+{
+	fast();
+
+	// test
+	{
+		solve();
+	}
+
+	return 0;
+}
